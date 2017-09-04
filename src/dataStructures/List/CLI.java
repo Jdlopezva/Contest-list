56fa3e203723f892d42c752ec3f9b4e5ca01a3ec
Jdlopezva <31075736+Jdlopezva@users.noreply.github.com>
2017-09-03 22:54:19 -0500

    Update CLI.java

--- src/dataStructures/List/CLI.java
+++ src/dataStructures/List/CLI.java
@@ -10,10 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-/**
- *
 
- */
 public class CLI {
     
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
